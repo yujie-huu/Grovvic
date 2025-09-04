a632a6a12170cdaa8b6aa215ec3c665c87b3f89b
Yujie <yhuu0149@student.monash.edu>
2025-09-05 00:58:47 +1000

    re-add password protection, fix the problem of landing detail page at bottom

---
+++ netzero/middleware.js
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req) {
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASS
+    ) {
+      return NextResponse.next()
+    }
+  }
+
+  return new Response('Auth required', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
